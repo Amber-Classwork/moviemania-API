a86bba959d6e58dca92489b0a6328feca09d6b5e
aldaine-AG <aldaineclarke1@gmail.com>
2022-12-06 21:58:21 -0500

    created a response for all movies by making multiple requests to the tmdb api

--- api/v1/controllers/movie.controller.js
+++ api/v1/controllers/movie.controller.js
@@ -3,13 +3,54 @@ const Movie = require("../../../schemas/movie.schema");
 const axios = require("axios");
 const {TMDB_KEY} = process.env;
 
+const movieGenres = [
+    { id: 28, name: 'Action' },
+    { id: 12, name: 'Adventure' },
+    { id: 16, name: 'Animation' },
+    { id: 35, name: 'Comedy' },
+    { id: 80, name: 'Crime' },
+    { id: 99, name: 'Documentary' },
+    { id: 18, name: 'Drama' },
+    { id: 10751, name: 'Family' },
+    { id: 14, name: 'Fantasy' },
+    { id: 36, name: 'History' },
+    { id: 27, name: 'Horror' },
+    { id: 10402, name: 'Music' },
+    { id: 9648, name: 'Mystery' },
+    { id: 10749, name: 'Romance' },
+    { id: 878, name: 'Science Fiction' },
+    { id: 10770, name: 'TV Movie' },
+    { id: 53, name: 'Thriller' },
+    { id: 10752, name: 'War' },
+    { id: 37, name: 'Western' }
+  ]
 class MovieController{
 
 
     static getAllMovies = async(req, res, next)=>{
         try{
+            // let genresResponse = await axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${TMDB_KEY}&append_to_response=videos`);
+            // console.log(genresResponse.data)
+            console.time("response")
+
+            let comedy = await axios.get(`https://api.themoviedb.org/3/discover/movie/?api_key=${TMDB_KEY}&append_to_response=videos&with_genres=35`);
+            let action = await axios.get(`https://api.themoviedb.org/3/discover/movie/?api_key=${TMDB_KEY}&append_to_response=videos&with_genres=28`); 
+            let animation = await axios.get(`https://api.themoviedb.org/3/discover/movie/?api_key=${TMDB_KEY}&append_to_response=videos&with_genres=16`);
+            let romance = await axios.get(`https://api.themoviedb.org/3/discover/movie/?api_key=${TMDB_KEY}&append_to_response=videos&with_genres=10749`);
+            let fantasy = await axios.get(`https://api.themoviedb.org/3/discover/movie/?api_key=${TMDB_KEY}&append_to_response=videos&with_genres=14`);
+            let drama = await axios.get(`https://api.themoviedb.org/3/discover/movie/?api_key=${TMDB_KEY}&append_to_response=videos&with_genres=18`);
             let response = await axios.get(`https://api.themoviedb.org/3/discover/movie/?api_key=${TMDB_KEY}&append_to_response=videos`);
-            JSONResponse.success(res, "Successfully retrieved all movies", response.data, 200);
+            console.timeEnd("response")
+            let data = {
+                popular: response.data,
+                comedy: comedy.data,
+                action: action.data,
+                animation: animation.data,
+                romance: romance.data,
+                fantasy: fantasy.data,
+                drama: drama.data,
+            }
+            JSONResponse.success(res, "Successfully retrieved all movies", data, 200);
         }catch(error){
             JSONResponse.error(res, "Unable to retrieve movies", error, 404);
         }
